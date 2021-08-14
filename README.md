7da4675b6ad05db3286b5bd8355146bf192faa0e
Elio Lu <1520622848@qq.com>
2021-08-14 13:50:26 +0800

    :octocat:

---
+++ README.md
@@ -0,0 +1,25 @@
+# fox-game-vue-v4
+# vue2 frames old fox game, also because i can't make it work with vue3, either vue3+typescript
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <!-- <p>FOX GAME v2.0 (vue2)</p> -->
+  <div class="container">
+    <div class="inner">
+      <div class="game day"></div>
+      <div class="fox hidden"></div>
+      <div class="poop-bag hidden"></div>
+      <div class="foreground-rain"></div>
+      <div class="frame"></div>
+      <div class="modal">
+        <div class="modal-inner">Press the middle button to start</div>
+      </div>
+      <div class="buttons">
+        <div class="btn left-btn"></div>
+        <div class="btn middle-btn"></div>
+        <div class="btn right-btn"></div>
+      </div>
+      <div class="icons">
+        <div class="icon highlighted fish-icon"></div>
+        <div class="icon poop-icon"></div>
+        <div class="icon weather-icon"></div>
+      </div>
+    </div>
+  </div>
+</template>late>
+
+<script>
+import gameState, { handleUserAction } from "./gameState.js";
+import { TICK_RATE } from "./constants.js";
+import initButtons from "./buttons.js";
+export default {
+  name: "App",
+  data() {
+    return {
+      nextTimeToTick: Date
+    };
+  },
+  created() {
+    this.nextTimeToTick = Date.now();
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    async init() {
+      console.log("starting game");
+      initButtons(handleUserAction);
+      requestAnimationFrame(this.nextAnimationFrame);
+    },
+    async nextAnimationFrame() {
+      const now = Date.now();
+
+      if (this.nextTimeToTick <= now) {
+        gameState.tick();
+        this.nextTimeToTick = now + TICK_RATE;
+      }
+      requestAnimationFrame(this.nextAnimationFrame);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<style>
+@import url("./assets/sprites.css");
+@import url("./assets/style.css");
+</style>
